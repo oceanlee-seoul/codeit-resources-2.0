8edca4b3e0823099cd1f8249b026ee632c365c1a
tkddbs587 <162422277+tkddbs587@users.noreply.github.com>
2024-11-08 13:24:32 +0900

    ♻️ Refactor(#72): 토스트 떠있을 때 다른 버튼 안눌림 현상 수정 (#73)

--- src/components/commons/Toast/index.tsx
+++ src/components/commons/Toast/index.tsx
@@ -1,8 +1,7 @@
-import CheckIcon from "@/../public/icons/icon-check-bold.svg";
 import { ToastProps, deleteToastAtom } from "@/store/toastAtom";
 import { useSetAtom } from "jotai";
+import Image from "next/image";
 import { useEffect, useState } from "react";
-import { PiExclamationMark } from "react-icons/pi";
 
 const DURATION_TIME = 2000;
 
@@ -28,19 +27,24 @@ function Toast({ id, type, message = "테스트 메시지 확인!" }: ToastProps
 
   return (
     <div
-      className={`transition-all duration-300 ease-in-out ${
+      className={`pointer-events-auto transition-all duration-300 ease-in-out ${
         isVisible ? "translate-y-0 opacity-100" : "translate-y-2 opacity-0"
       }`}
     >
       <div
-        className={`flex w-max items-center gap-8 rounded-16 py-8 pl-16 pr-20 shadow-[0_4px_24px_0_rgba(0,0,0,0.2)] ${
+        className={`flex w-max items-center gap-8 rounded-16 px-16 py-8 shadow-[0_4px_24px_0_rgba(0,0,0,0.2)] ${
           type === "success" ? "bg-green-80" : "bg-red-500"
         }`}
       >
         {type === "success" ? (
-          <CheckIcon width={20} height={20} color="white" />
+          <Image
+            width={20}
+            height={20}
+            alt="성공 아이콘"
+            src="icons/icon-check-bold.svg"
+          />
         ) : (
-          <PiExclamationMark width={20} height={20} color="white" />
+          <span className="pr-4 text-17-700 text-white">!</span>
         )}
 
         <span className="text-17-500 text-white">{message}</span>
